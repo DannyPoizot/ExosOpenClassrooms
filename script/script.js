c6e26abe2b362b72d3197db8c8b01157e4f0b6fc
Danny Poizot <poizot.dan@gmail.com>
2021-04-12 14:35:48 +0200

    Premier commit

---
+++ script/script.js
@@ -0,0 +1,26 @@
+let parent = document.getElementById("parent");
+
+let child = document.getElementById("child");
+
+let cptParent = 0;
+let cptChild = 0;
+
+document.getElementById("parent-count").innerHTML = cptParent;
+document.getElementById("child-count").innerHTML = cptChild;
+
+parent.addEventListener("click", function(e){
+  cptParent++;
+ document.getElementById("parent-count").innerHTML =    cptParent;
+});
+
+child.addEventListener("click", function(e){
+  cptChild++;
+ document.getElementById("child-count").innerHTML =     cptChild;
+  event.stopPropagation();
+  event.preventDefault();
+});
+
+child.addEventListener("mouseover", function(e){
+  cptChild++;
+ document.getElementById("child-count").innerHTML =     cptChild;
+});
