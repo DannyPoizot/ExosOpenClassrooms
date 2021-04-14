f7026ce755aed30b51e7c0c6255b5df105273a7a
Danny Poizot <poizot.dan@gmail.com>
2021-04-14 14:57:28 +0200

    fin exercice envoie données

--- js/envoieDonnees.js
+++ js/envoieDonnees.js
@@ -1,25 +1,27 @@
-function send() {
+function send(e) {
   let request = new XMLHttpRequest();
 
   request.open("POST", "https://mockbin.com/request");
 
   request.setRequestHeader("Content-Type", "application/json");
 
+  e.preventDefault();
+  // let value = document.getElementById("value").value;
+  // console.log(value);
 
-  let value = document.getElementById("value").value;
-  console.log(value);
-
-  request.onreadystatechange = function() {
-      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        let response = JSON.stringify(json);
+  // request.onreadystatechange = function() {
+      // if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        let userResponse = document.getElementById("value").value;
+        // console.log(userResponse);
+        let response = JSON.stringify(userResponse);
+        request.send(response);
         console.log(response);
         let json = JSON.parse(response);
         console.log(json);
-        request.send(JSON.stringify(json));
-        document.getElementById("value").innerHTML = response;
-      }
-    }
+//       }
+//     }
+//
+        document.getElementById("result").innerHTML = json;
 
-}
-
-document.getElementById("result").addEventListener("click", send);
+ }
+document.getElementById("bouton").addEventListener("click", send);
