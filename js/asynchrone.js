1695a2d64eba9faffd4c77d27b6b0a4fea6fed21
Danny Poizot <poizot.dan@gmail.com>
2021-04-15 14:09:05 +0200

    fin asynchrone

--- js/asynchrone.js
+++ js/asynchrone.js
@@ -7,15 +7,15 @@ async function getNumber2() {
   return 4;
 }
 
-let promise = new Promise(() => {
+
   async function compute() {
-    let gN1 = await getNumber1;
-    let gN2 = await getNumber2;
+    let gN1 = await getNumber1();
+    let gN2 = await getNumber2();
     return gN1 + gN2;
   }
-});
-console.log(promise);
-promise.then(posts => {
+
+
+compute().then(posts => {
   document.getElementById("result").innerHTML = posts;
 })
 .catch(error => { });
