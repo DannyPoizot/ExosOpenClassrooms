585bf73721ebc7f1319f98e9968c8333d9f07031
Danny Poizot <poizot.dan@gmail.com>
2021-04-27 17:17:00 +0200

    carrousel JQuery

---
+++ js/carrouselJQuery.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+let $carrousel = $("#carrousel");
+let $img = $("#carrousel img");
+let index = $img.length - 1;
+let i = 0;
+let $currentImg = $img.eq(i);
+
+$img.css("display", "none");
+$currentImg.css("display", "block");
+
+
+$(".fa-arrow-right").click(function(){
+  i++;
+  if(i <= index){
+    $img.css("display", "none");
+    $currentImg = $img.eq(i);
+    $currentImg.css("display", "block");
+  } else {
+    i = index;
+  }
+});
+
+$(".fa-arrow-left").click(function(){
+  i--;
+  if(i >= 0){
+    $img.css("display", "none");
+    $currentImg = $img.eq(i);
+    $currentImg.css("display", "block");
+  } else {
+    i = 0;
+  }
+});
+
+function slideImg(){
+    setTimeout(function(){
+      if(i < index){
+	      i++;
+      } else {
+	       i = 0;
+	}
+
+	$img.css('display', 'none');
+
+	$currentImg = $img.eq(i);
+	$currentImg.css('display', 'block');
+
+	slideImg();
+
+}, 2000);
+}
+slideImg();
+});
